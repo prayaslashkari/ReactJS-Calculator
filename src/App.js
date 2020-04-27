b462d210c78932b9dd474144d9c1c420d65db207
Prayas Lashkari <rock198269@gmail.com>
2020-04-27 18:51:17 +0530

    Calculator

--- src/App.js
+++ src/App.js
@@ -1,26 +1,90 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Keypad from './components/Keypad.jsx'
+import Output from './components/Output.jsx'
+
+
+class App extends Component {
+
+  constructor(){
+      super();
+
+      this.state = {
+          result: ""
+      }
+  }
+
+  onClick = button => {
+
+      if(button === "="){
+          this.calculate()
+      }
+
+      else if(button === "C"){
+          this.reset()
+      }
+      else if(button === "CE"){
+          this.backspace()
+      }
+
+      else {
+          this.setState({
+              result: this.state.result + button
+          })
+      }
+  };
+
+
+  calculate = () => {
+      var checkResult = ''
+      if(this.state.result.includes('--')){
+          checkResult = this.state.result.replace('--','+')
+      }
+
+      else {
+          checkResult = this.state.result
+      }
+
+      try {
+          this.setState({
+              // eslint-disable-next-line
+              result: (eval(checkResult) || "" ) + ""
+          })
+      } catch (e) {
+          this.setState({
+              result: "error"
+          })
+
+      }
+  };
+
+  reset = () => {
+      this.setState({
+          result: ""
+      })
+  };
+
+  backspace = () => {
+      this.setState({
+          result: this.state.result.slice(0, -1)
+      })
+  };
+
+  render() {
+      return (
+          <div>
+              
+              <div className="calculator-body">
+                  <h1>Simple Calculator</h1>
+                  <Output result={this.state.result}/>
+                  <Keypad onClick={this.onClick}/>
+              </div>
+
+          </div>
+      );
+  }
 }
 
 export default App;
---
+++ src/components/Output.jsx
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class Output extends Component {
+
+
+    render() {
+        let {result} = this.props;
+        return (
+            <div className="result">
+                <p>{result}</p>
+            </div>
+    )
+        ;
+    }
+}
+
+
+export default Output;
